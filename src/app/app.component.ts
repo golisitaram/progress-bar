14ef4ef10eba96f96899779b0df3bf033f4bf55d
golisitaram <goli.seetaram@gmail.com>
2017-07-14 01:05:11 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,20 @@ export class AppComponent {
     bars : [],
     buttons : []
   }
+  selectedOpt :any = 0 ;
   getDynamicData(){
     this.http.get('http://pb-api.herokuapp.com/bars')
     .subscribe(
       (res) =>{
-        this.data = res.json();;
+        this.data = res.json();
+        this.selectedOpt = 0;
       }
     )
   };
   ngOnInit() {
     this.getDynamicData()
   }
-  selectedOpt :any = 0 ;
+  
 
   updateProgress(b){
     this.data.bars[this.selectedOpt] = (this.data.bars[this.selectedOpt] + b > this.data.limit) ? this.data.limit : this.data.bars[this.selectedOpt] + b;
